3ef9ae0b16efa962428e580dfc82d9a42451c0ed
giorgi gogoladze <gogoladzeg321@gmail.com>
2023-07-11 15:58:45 +0400

    first commit

---
+++ schemas/furniture.ts
@@ -0,0 +1,88 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'furniture',
+  title: 'furniture',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: rule => rule.required(),
+    }),
+    defineField({
+      title: 'Description',
+      name: 'description',
+      type: 'text',
+      validation: rule => rule.required(),
+    }),
+    {
+      title: 'min price',
+      validation: rule => rule.min(50).max(1000).required(),
+      name: 'minPrice',
+      type: 'number'
+    },
+    {
+      title: 'max price',
+      validation: rule => rule.min(100).max(1200).required(),
+      name: 'maxPrice',
+      type: 'number'
+    },
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: rule => rule.required(),
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: rule => rule.required(),
+    }),
+    defineField({
+      name: 'whiteImage',
+      title: 'white image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: rule => rule.required(),
+    }),
+    defineField({
+      name: 'grayImage',
+      title: 'gray image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: rule => rule.required(),
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'reference',
+      to: [{type: 'category'}],
+      validation: rule => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+      validation: rule => rule.required(),
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+})
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,5 @@
 import blockContent from './blockContent'
 import category from './category'
-import post from './post'
-import author from './author'
+import furniture from './furniture'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [furniture, category, blockContent]
